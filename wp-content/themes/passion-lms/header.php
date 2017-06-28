e33f846d0a3ea65be36a7f0660715f2319cbe080
jessephillips <mr.jesse.phillips@gmail.com>
2017-06-28 15:32:55 -0400

    Add a base site header.

--- wp-content/themes/passion-lms/header.php
+++ wp-content/themes/passion-lms/header.php
@@ -9,6 +9,8 @@
  * @package Passion_LMS
  */
 
+$page_title = get_the_title();
+
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -16,39 +18,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.css">
+
+<title><?php echo $page_title; ?></title>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
+	<?php include('navigation.php'); ?>
+
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'passion-lms' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'passion-lms' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
---
+++ wp-content/themes/passion-lms/navigation.php
@@ -0,0 +1,5 @@
+<nav class="navbar background-lightgray">
+  <div class="container text-center">
+    <h1>The Top Nav to be replaced by Passion Nav?</h1>
+  </div>
+</nav>
