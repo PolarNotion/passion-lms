767ee64762d2a580b4fea5073e5330ce62580744
jessephillips <mr.jesse.phillips@gmail.com>
2017-06-29 11:19:10 -0400

    Add modal pop-ups to Grid Section module.

--- wp-content/themes/passion-lms/functions.php
+++ wp-content/themes/passion-lms/functions.php
@@ -111,6 +111,12 @@ function passion_lms_scripts() {
 
 	wp_enqueue_script( 'passion-lms-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_register_script( 'my-jquery' , 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' );
+	wp_enqueue_script( 'my-jquery');
+
+	wp_register_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.js', array('my-jquery'), '', true);
+	wp_enqueue_script( 'bootstrap-js');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
--- wp-content/themes/passion-lms/modules/grid_section.php
+++ wp-content/themes/passion-lms/modules/grid_section.php
@@ -11,7 +11,11 @@
     <div class="row">
 			<?php if(get_sub_field('content_items')): ?>
 
-        <?php while(the_repeater_field('content_items')): ?>
+        <?php
+          $modal_id = 0;
+          while(the_repeater_field('content_items')):
+            $modal_id += 1;
+        ?>
 
           <div class="col-sm-3 js-height">
             <?php
@@ -22,10 +26,31 @@
 						  $other_file  = get_sub_field('other_file');
 	          ?>
             <div class="vertical-child fixedratio position-relative" style="background-image: url('<?php echo $image; ?>');">
-              <img class="file-icon" src="<?php echo get_template_directory_uri() . '/img/play.svg' ?>">
+              <a href="#" data-toggle="modal" data-target="#modal_<?php echo $modal_id; ?>"><img class="file-icon" src="<?php echo get_template_directory_uri() . '/img/play.svg' ?>"></a>
             </div>
             <div>
               <?php echo $title; ?>
+            </div>
+
+            <!-- Modal -->
+            <div id="modal_<?php echo $modal_id; ?>" class="modal fade" role="dialog">
+              <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Modal Header</h4>
+                  </div>
+                  <div class="modal-body">
+                    <p><?php echo $description; ?></p>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  </div>
+                </div>
+
+              </div>
             </div>
           </div>
 
