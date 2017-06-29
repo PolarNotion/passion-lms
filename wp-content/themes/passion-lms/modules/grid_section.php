83005a1bec00a3bf9ca83a6931d77e0f5e4ec95b
jessephillips <mr.jesse.phillips@gmail.com>
2017-06-29 13:44:12 -0400

    Add the Show More button.

--- wp-content/themes/passion-lms/modules/grid_section.php
+++ wp-content/themes/passion-lms/modules/grid_section.php
@@ -1,6 +1,7 @@
 <?php
-	$title		     = get_sub_field('title');
-	$access        = get_sub_field('access');
+	$title   = get_sub_field('title');
+	$access  = get_sub_field('access');
+  $row_id  = get_row_index();
 ?>
 
 <section class="text-center pad">
@@ -15,16 +16,19 @@
           $modal_id = 0;
           while(the_repeater_field('content_items')):
             $modal_id += 1;
+            if($modal_id == 9) {
+              echo "</div><a href='#more_" . $row_id . "' class='btn btn-primary' data-toggle='collapse'>Show More</a><div class='row collapse' id='more_" . $row_id . "'>";
+            }
         ?>
 
           <div class="col-sm-3 js-height">
-            <?php
-            	$title       = get_sub_field('title');
-						  $image       = get_sub_field('image');
-							$description = get_sub_field('description');
-						  $video_file  = get_sub_field('video_file');
-						  $other_file  = get_sub_field('other_file');
-	          ?>
+          <?php
+          	$title       = get_sub_field('title');
+					  $image       = get_sub_field('image');
+						$description = get_sub_field('description');
+					  $video_file  = get_sub_field('video_file');
+					  $other_file  = get_sub_field('other_file');
+          ?>
             <div class="vertical-child fixedratio position-relative" style="background-image: url('<?php echo $image; ?>');">
               <a href="#" data-toggle="modal" data-target="#modal_<?php echo $modal_id; ?>"><img class="file-icon" src="<?php echo get_template_directory_uri() . '/img/play.svg' ?>"></a>
             </div>
@@ -51,11 +55,11 @@
                 </div>
 
               </div>
-            </div>
+            </div><!-- end Modal -->
           </div>
 
-				<?php endwhile; ?>
-      <?php endif; ?>
+				<?php endwhile;
+              endif; ?>
     </div>
   </div>
 </section>
