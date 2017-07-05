47ef659be361ad32afa1569b4a994cc43548f8fc
jessephillips <mr.jesse.phillips@gmail.com>
2017-07-05 15:40:02 -0400

    Stub the mobile navigation pop-up.

--- wp-content/themes/passion-lms/js/general.js
+++ wp-content/themes/passion-lms/js/general.js
@@ -3,7 +3,7 @@
  *
  */
 
-
+// Smooth scrolling to targets on the same page
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
@@ -17,3 +17,12 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+
+// Open collapsed navigation on mobile
+$(".toggleable").click(function(){
+  var target = $(this).data('target');
+
+  $(target).fadeToggle();
+
+  return false;
+})
