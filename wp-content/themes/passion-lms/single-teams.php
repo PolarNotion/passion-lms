7ab9a024b51debadd2f66a6dd71fc13d3e670f59
jessephillips <mr.jesse.phillips@gmail.com>
2017-07-17 16:35:16 -0400

    Show blog posts with the proper Team ID.

--- wp-content/themes/passion-lms/single-teams.php
+++ wp-content/themes/passion-lms/single-teams.php
@@ -10,6 +10,7 @@ $title 				= get_the_title();
 $description 	= get_field('team_description');
 $access_list 	= get_field('access_list');
 $permissions_list = explode(" ", $access_list);
+$team_id			= get_the_ID();
 
 if (pn_user_has_access($permissions_list)):
 
@@ -23,12 +24,46 @@ get_header(); ?>
 			echo $title;
 			echo "<br>";
 			echo $description;
+			echo "<br>";
+			echo $team_id;
 
-			while ( have_posts() ) : the_post();
+			// args
+			$args = array(
+				'posts_per_page'	=> 3,
+				'meta_query'	=> array(
+					array(
+							'key'		=> 'view_access',
+							'value'		=> serialize( strval($team_id) ),
+							'compare'	=> 'LIKE'
+					)
+				)
+			);
+		  $loop = new WP_Query( $args );
+
+			while ( $loop->have_posts() ) : $loop->the_post();
+	      $featured_image = get_field('featured_image');
+				$view_access_list = get_field('view_access');
+	    ?>
+	      <div class="div">
+	        <div class="blog-teaser">
+	          <?php if($featured_image != ''): ?>
+	              <div class="blog-image">
+	                <div class="fixedratio" style="background-image: url('<?php echo $featured_image; ?>')"></div>
+	              </div>
+	          <?php endif; ?>
+	          <?php the_title(); ?>
+	          <?php the_excerpt(); ?>
+						<?php print_r($view_access_list); ?>
+	          <a href="<?php the_permalink(); ?>">Read More</a>
+	        </div>
+	      </div>
+
+	    <?php endwhile;
+	          wp_reset_postdata(); // this is necessary in order to run another query in another module on the same page
+
 
 				get_template_part( 'template-parts/content', 'page' );
 
-			endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
