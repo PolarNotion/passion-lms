82da3255763393686de922b8652686dded8382aa
jessephillips <mr.jesse.phillips@gmail.com>
2017-07-18 13:33:33 -0400

    Remove Production Blog custom post type.

--- wp-content/themes/passion-lms/functions.php
+++ wp-content/themes/passion-lms/functions.php
@@ -133,32 +133,6 @@ add_action( 'widgets_init', 'passion_lms_widgets_init' );
 	}
 	add_action( 'init', 'custom_post_announcements' );
 
-	function custom_post_production_blog() {
-		 $labels = array(
-			 'name' 					=> _x( 'Production Blog', 'post type general name' ),
-			 'singular_name' 	=> _x( 'Production Blog Post', 'post type singular name' ),
-			 'add_new' 				=> _x( 'Add New', 'book'),
-			 'add_new_item'		=> __( 'Add New Blog Post' ),
-			 'edit_item'			=> __( 'Edit Blog Post' ),
-			 'new_item'				=> __( 'New Blog Post' ),
-			 'all_items'			=> __( 'All Production Blog Posts' ),
-			 'view_item'			=> __( 'View Blog Post' ),
-			 'search_items'		=> __( 'Search Production Blog Posts' ),
-			 'menu_name'			=> 'Production Blog'
-		 );
-		 $args = array (
-			 'labels'				=> $labels,
-			 'description'	=> 'Blog Posts for Production Volunteers',
-			 'public'				=> true,
-			 'menu_position' => 5,
-			 'supports'			=> array ('title', 'page-attributes' ),
-			 'has_archive' 	=> true,
-			 'taxonomies' 	=> array( 'category' ),
-		 );
-	 register_post_type( 'production_blog', $args );
-	}
-	add_action( 'init', 'custom_post_production_blog' );
-
 	function custom_post_teams() {
 		 $labels = array(
 			 'name' 					=> _x( 'Teams', 'post type general name' ),
