7b150132e89f8404d067e08cb5838f126fd27d44
jessephillips <mr.jesse.phillips@gmail.com>
2017-07-19 14:44:21 -0400

    Add Announcements with no team to Dashboard.

--- wp-content/themes/passion-lms/dashboard.php
+++ wp-content/themes/passion-lms/dashboard.php
@@ -13,7 +13,55 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 			<?php
+			// announcement args
+			$announcement_args = array(
+				'posts_per_page'	=> -1,
+				'post_type' => 'announcements',
+				'meta_query'	=> array(
+					array(
+							'key'		=> 'view_access',
+							'value'	=> '',
+							'compare'	=> '='
+					)
+				)
+			);
 
+			$announcement_loop = new WP_Query( $announcement_args );
+
+			if ( $announcement_loop->have_posts() ) :
+			?>
+				<section class="section-spacing">
+					<?php
+					$item_no = 0;
+					while ( $announcement_loop->have_posts() ) : $announcement_loop->the_post();
+						$title = get_field('title');
+						$the_text = get_field('announcement_text');
+						$button_text = get_field('button_text');
+						$button_link = get_field('button_link');
+
+						$pad_top = '';
+						$item_no += 1;
+						if ($item_no > 1) {
+							$pad_top = 'pad-top';
+						}
+						?>
+						<div class="container">
+							<div class="announcement <?php echo $pad_top; ?>">
+								<h3><?php echo $title; ?><?php echo get_the_ID(); ?> &nbsp;<i class="fa fa-bullhorn fa-lg" aria-hidden="true"></i></h3>
+								<?php echo $the_text; ?>
+								<?php if ($button_text != ''): ?>
+									<a href="<?php echo $button_link; ?>" class="btn btn-secondary-white btn-lg"><?php echo $button_text; ?></a>
+								<?php endif; ?>
+							</div>
+						</div>
+					<?php endwhile;
+								wp_reset_postdata(); // this is necessary in order to run another query in another module on the same page
+					?>
+					</section>
+				<?php endif; ?>
+
+			<?php
+			// TEAM Index module
 			$args = array(
 			  'posts_per_page'	=> -1,
 			  'post_type'   => 'teams',
@@ -22,8 +70,8 @@ get_header(); ?>
 			$loop = new WP_Query( $args );
 			?>
 
-			<section>
-			  <div class="container pad">
+			<section class="section-spacing">
+			  <div class="container">
 			      <div class="row">
 			        <?php while ( $loop->have_posts() ) : $loop->the_post();
 			          $access_list  = get_field('access_list');
