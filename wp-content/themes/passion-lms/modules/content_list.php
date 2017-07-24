f8df9f2d45dc7fbad818594ca0210be1277e5b89
jessephillips <mr.jesse.phillips@gmail.com>
2017-07-24 12:31:21 -0400

    Add New Tag to Content List module.

--- wp-content/themes/passion-lms/modules/content_list.php
+++ wp-content/themes/passion-lms/modules/content_list.php
@@ -24,6 +24,8 @@
               $video_file  = get_sub_field('video_file');
 							$audio_file	 = get_sub_field('audio_file');
               $other_file  = get_sub_field('other_file');
+							$date_posted	= get_sub_field('date_posted');
+							$one_week_ago = date_create('-1 week');
 
               // This include sets $file_icon to the right icon file
   						$video_file = $video_file;
@@ -42,6 +44,7 @@
                 <div data-toggle="tab" data-target="#item_<?php echo $item_no; ?>">
                   <div class="content-trigger clearfix">
                     <div class="content-thumbnail pull-left js-height" style="background-image: url('<?php echo $image; ?>')">
+											<?php if($date_posted > date_format($one_week_ago, 'Ymd') ): ?><div class="new-tag-inline">New</div><?php endif; ?>
                     </div>
                     <div class="title js-height">
 												<div class="truncate">
