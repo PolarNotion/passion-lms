2976f5fdbfe9db7870df769539d05083c0922e41
jessephillips <mr.jesse.phillips@gmail.com>
2017-10-13 13:54:02 -0400

    Style the Admin Panel.

--- wp-content/themes/passion-lms/functions.php
+++ wp-content/themes/passion-lms/functions.php
@@ -301,9 +301,52 @@ function pn_user_has_access ($allowed_permissions) {
   return $user_has_access;
 }
 
+//
+// ACF Stuff
+//
 // Add an Options page for the theme
 if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page();
 
 }
+
+function my_acf_admin_head() {
+	?>
+	<style type="text/css">
+
+		.acf-flexible-content .layout .acf-fc-layout-handle {
+			/*background-color: #00B8E4;*/
+			background-color: #202428;
+			color: #eee;
+		}
+
+		.acf-repeater.-row > table > tbody > tr > td,
+		.acf-repeater.-block > table > tbody > tr > td {
+			border-top: 2px solid #202428;
+		}
+
+		.acf-repeater .acf-row-handle {
+			vertical-align: top !important;
+			padding-top: 16px;
+		}
+
+		.acf-repeater .acf-row-handle span {
+			font-size: 20px;
+			font-weight: bold;
+			color: #202428;
+		}
+
+		.imageUpload img {
+			width: 75px;
+		}
+
+		.acf-repeater .acf-row-handle .acf-icon.-minus {
+			top: 30px;
+		}
+
+	</style>
+	<?php
+}
+
+add_action('acf/input/admin_head', 'my_acf_admin_head');
